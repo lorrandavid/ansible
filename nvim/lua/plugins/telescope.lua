c81e448581e96e2cea04a4cfa25ec827c13f2ae7
lorran.thierry.david <lorran.thierry.david@accenture.com>
2023-09-08 15:31:41 -0300

    fix keymaps for telescope & update work script to get nvim configs

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -9,5 +9,20 @@ return {
         require("telescope").load_extension("fzf")
       end,
     },
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>" },
+      { "<C-p>", "<cmd>Telescope git_files<cr>" },
+      { "<leader>fw", "<cmd>Telescope live_grep<cr>" },
+    },
   },
+  -- {
+  --   "nvim-telescope/telescope.nvim",
+  --   opts = {
+  --     pickers = {
+  --       find_files = {
+  --
+  --       }
+  --     }
+  --   }
+  -- }
 }

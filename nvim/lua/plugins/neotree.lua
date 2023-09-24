e0f45ed38f68fc88d8328f596685569a9bcdd899
Lorran David <lorrantd1@gmail.com>
2023-09-24 01:09:36 -0300

    feat: setup neotree toggle left

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -2,6 +2,7 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   keys = {
     { "<leader>e", ":Neotree toggle float<CR>", silent = true, desc = "Float File Explorer" },
+    { "<leader>fe", ":Neotree toggle left<CR>", silent = true, desc = "Left File Explorer" },
   },
   opts = {
     filesystem = {

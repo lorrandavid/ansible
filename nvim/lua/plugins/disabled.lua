e5aa609cf3d6e7850da937ccab1f4af993ba8028
Lorran David <lorrantd1@gmail.com>
2023-10-12 20:56:06 -0300

    move on

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,6 +1,4 @@
 return {
   { "echasnovski/mini.pairs", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
-  { "zbirenbaum/copilot.lua", enabled = false },
-  { "goolord/alpha-nvim", enabled = false },
 }

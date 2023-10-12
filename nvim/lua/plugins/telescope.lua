3edc6e20e9fa411081ccff73462873bbea2b32ad
Lorran David <lorrantd1@gmail.com>
2023-10-12 02:21:39 -0300

    telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
     dependencies = {
       "nvim-telescope/telescope-fzf-native.nvim",
       cmd = "Telescope",
-      build = "make",
+      build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
       config = function()
         require("telescope").load_extension("fzf")
       end,

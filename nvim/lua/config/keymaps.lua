2dc10b7b27a9749f0af13f29f848a63585dd2fd0
Lorran David <lorrantd1@gmail.com>
2023-10-16 18:10:56 -0300

    add toggleterm

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -16,9 +16,27 @@ local function map(mode, lhs, rhs, opts)
   end
 end
 
+-- Better navigation for Tmux
 map("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<cr>")
 map("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<cr>")
 map("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<cr>")
 map("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<cr>")
 map("n", "<C-\\>", "<Cmd>NvimTmuxNavigateLastActive<cr>")
 map("n", "<C-Space>", "<Cmd>NvimTmuxNavigateNext<cr>")
+
+-- Toggle term
+map("i", [[<C-\>]], '<cmd>exe v:count1 . "ToggleTerm"<CR>')
+map("n", [[<C-\>]], '<cmd>exe v:count1 . "ToggleTerm"<CR>')
+
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0, noremap = true }
+  vim.keymap.set("t", "<esc>", [[<C-\><C-n><C-w>l]], opts)
+  vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+end
+
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Enable smart indenting (see https://stackoverflow.com/questions/1204149/smart-wrap-in-vim)
+vim.opt.breakindent = true
+
+--- Always keep 8 lines above/below cursor unless at start/end of file
+vim.opt.scrolloff = 8
+
+-- Place a column line
+vim.opt.colorcolumn = "80"

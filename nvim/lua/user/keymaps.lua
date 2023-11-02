cdaef522c47b9560ae762eb13617456252dd109b
Lorran David <lorrantd1@gmail.com>
2023-11-01 22:21:59 -0300

    change neotree to float

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -29,39 +29,39 @@ nnoremap("<leader>gg", "<cmd>LazyGit<CR>", { desc = "Open Lazygit" })
 
 -- Better tmux navigation
 nnoremap("<C-j>", function()
-	if vim.fn.exists(":NvimTmuxNavigateDown") ~= 0 then
-		vim.cmd.NvimTmuxNavigateDown()
-	else
-		vim.cmd.wincmd("j")
-	end
+  if vim.fn.exists(":NvimTmuxNavigateDown") ~= 0 then
+    vim.cmd.NvimTmuxNavigateDown()
+  else
+    vim.cmd.wincmd("j")
+  end
 end)
 
 nnoremap("<C-k>", function()
-	if vim.fn.exists(":NvimTmuxNavigateUp") ~= 0 then
-		vim.cmd.NvimTmuxNavigateUp()
-	else
-		vim.cmd.wincmd("k")
-	end
+  if vim.fn.exists(":NvimTmuxNavigateUp") ~= 0 then
+    vim.cmd.NvimTmuxNavigateUp()
+  else
+    vim.cmd.wincmd("k")
+  end
 end)
 
 nnoremap("<C-l>", function()
-	if vim.fn.exists(":NvimTmuxNavigateRight") ~= 0 then
-		vim.cmd.NvimTmuxNavigateRight()
-	else
-		vim.cmd.wincmd("l")
-	end
+  if vim.fn.exists(":NvimTmuxNavigateRight") ~= 0 then
+    vim.cmd.NvimTmuxNavigateRight()
+  else
+    vim.cmd.wincmd("l")
+  end
 end)
 
 nnoremap("<C-h>", function()
-	if vim.fn.exists(":NvimTmuxNavigateLeft") ~= 0 then
-		vim.cmd.NvimTmuxNavigateLeft()
-	else
-		vim.cmd.wincmd("h")
-	end
+  if vim.fn.exists(":NvimTmuxNavigateLeft") ~= 0 then
+    vim.cmd.NvimTmuxNavigateLeft()
+  else
+    vim.cmd.wincmd("h")
+  end
 end)
 
 -- Map neo-tree to <leader>e
-nnoremap("<leader>e", "<cmd>Neotree toggle<cr>")
+nnoremap("<leader>e", "<cmd>Neotree toggle position=float<cr>")
 
 -- Focus on current buffer in neo-tree
 nnoremap("<leader>E", "<cmd>Neotree action=focus<cr>")
@@ -83,24 +83,24 @@ nnoremap("#", "#zz")
 
 -- Press 'S' for quick find/replace for the word under the cursor
 nnoremap("S", function()
-	local cmd = ":%s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>"
-	local keys = vim.api.nvim_replace_termcodes(cmd, true, false, true)
-	vim.api.nvim_feedkeys(keys, "n", false)
+  local cmd = ":%s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>"
+  local keys = vim.api.nvim_replace_termcodes(cmd, true, false, true)
+  vim.api.nvim_feedkeys(keys, "n", false)
 end)
 
 -- Open Spectre for global find/replace
 nnoremap("<leader>S", function()
-	require("spectre").toggle()
+  require("spectre").toggle()
 end)
 
 -- Open Spectre for global find/replace for the word under the cursor in normal mode
 nnoremap("<leader>sw", function()
-	require("spectre").open_visual({ select_word = true })
+  require("spectre").open_visual({ select_word = true })
 end, { desc = "Search current word" })
 
 -- Open Spectre for global find/replace for the word under the cursor in visual mode
 vnoremap("<leader>sw", function()
-	require("spectre").open_visual({ select_word = true })
+  require("spectre").open_visual({ select_word = true })
 end, { desc = "Search current word" })
 
 -- Press 'H', 'L' to jump to start/end of a line (first/last char)
@@ -117,44 +117,44 @@ nnoremap("<leader>no", "<cmd>noh<cr>")
 
 -- Goto next diagnostic of any severity
 nnoremap("]d", function()
-	vim.diagnostic.goto_next({})
-	vim.api.nvim_feedkeys("zz", "n", false)
+  vim.diagnostic.goto_next({})
+  vim.api.nvim_feedkeys("zz", "n", false)
 end)
 
 -- Goto previous diagnostic of any severity
 nnoremap("[d", function()
-	vim.diagnostic.goto_prev({})
-	vim.api.nvim_feedkeys("zz", "n", false)
+  vim.diagnostic.goto_prev({})
+  vim.api.nvim_feedkeys("zz", "n", false)
 end)
 
 -- Goto next error diagnostic
 nnoremap("]e", function()
-	vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
-	vim.api.nvim_feedkeys("zz", "n", false)
+  vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
+  vim.api.nvim_feedkeys("zz", "n", false)
 end)
 
 -- Goto previous error diagnostic
 nnoremap("[e", function()
-	vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
-	vim.api.nvim_feedkeys("zz", "n", false)
+  vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
+  vim.api.nvim_feedkeys("zz", "n", false)
 end)
 
 -- Goto next warning diagnostic
 nnoremap("]w", function()
-	vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN })
-	vim.api.nvim_feedkeys("zz", "n", false)
+  vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN })
+  vim.api.nvim_feedkeys("zz", "n", false)
 end)
 
 -- Goto previous warning diagnostic
 nnoremap("[w", function()
-	vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN })
-	vim.api.nvim_feedkeys("zz", "n", false)
+  vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN })
+  vim.api.nvim_feedkeys("zz", "n", false)
 end)
 
 nnoremap("<leader>d", function()
-	vim.diagnostic.open_float({
-		border = "rounded",
-	})
+  vim.diagnostic.open_float({
+    border = "rounded",
+  })
 end)
 
 -- Place all dignostics into a qflist
@@ -189,62 +189,62 @@ nnoremap("<leader>f", ":Format<cr>")
 -- Harpoon keybinds --
 -- Open harpoon ui
 nnoremap("<C-e>", function()
-	harpoon_ui.toggle_quick_menu()
+  harpoon_ui.toggle_quick_menu()
 end)
 
 -- Add current file to harpoon
 nnoremap("<leader>a", function()
-	harpoon_mark.add_file()
+  harpoon_mark.add_file()
 end)
 
 -- Quickly jump to harpooned files
 nnoremap("<leader>1", function()
-	harpoon_ui.nav_file(1)
+  harpoon_ui.nav_file(1)
 end)
 
 nnoremap("<leader>2", function()
-	harpoon_ui.nav_file(2)
+  harpoon_ui.nav_file(2)
 end)
 
 nnoremap("<leader>3", function()
-	harpoon_ui.nav_file(3)
+  harpoon_ui.nav_file(3)
 end)
 
 nnoremap("<leader>4", function()
-	harpoon_ui.nav_file(4)
+  harpoon_ui.nav_file(4)
 end)
 
 nnoremap("<leader>5", function()
-	harpoon_ui.nav_file(5)
+  harpoon_ui.nav_file(5)
 end)
 
 -- Git keymaps --
 nnoremap("<leader>gb", ":Gitsigns toggle_current_line_blame<cr>")
 nnoremap("<leader>gf", function()
-	local cmd = {
-		"sort",
-		"-u",
-		"<(git diff --name-only --cached)",
-		"<(git diff --name-only)",
-		"<(git diff --name-only --diff-filter=U)",
-	}
+  local cmd = {
+    "sort",
+    "-u",
+    "<(git diff --name-only --cached)",
+    "<(git diff --name-only)",
+    "<(git diff --name-only --diff-filter=U)",
+  }
 
-	if not utils.is_git_directory() then
-		vim.notify(
-			"Current project is not a git directory",
-			vim.log.levels.WARN,
-			{ title = "Telescope Git Files", git_command = cmd }
-		)
-	else
-		require("telescope.builtin").git_files()
-	end
+  if not utils.is_git_directory() then
+    vim.notify(
+      "Current project is not a git directory",
+      vim.log.levels.WARN,
+      { title = "Telescope Git Files", git_command = cmd }
+    )
+  else
+    require("telescope.builtin").git_files()
+  end
 end, { desc = "Search [G]it [F]iles" })
 
 -- Telescope keybinds --
 nnoremap("<leader>?", require("telescope.builtin").oldfiles, { desc = "[?] Find recently opened files" })
 nnoremap("<leader>sb", require("telescope.builtin").buffers, { desc = "[S]earch Open [B]uffers" })
 nnoremap("<leader>sf", function()
-	require("telescope.builtin").find_files({ hidden = true })
+  require("telescope.builtin").find_files({ hidden = true })
 end, { desc = "[S]earch [F]iles" })
 nnoremap("<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
 nnoremap("<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
@@ -253,32 +253,32 @@ nnoremap("<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]ea
 nnoremap("<leader>sd", require("telescope.builtin").git_files, { desc = "[S]earch [D]iagnostics" })
 
 nnoremap("<leader>sc", function()
-	require("telescope.builtin").commands(require("telescope.themes").get_dropdown({
-		previewer = false,
-	}))
+  require("telescope.builtin").commands(require("telescope.themes").get_dropdown({
+    previewer = false,
+  }))
 end, { desc = "[S]earch [C]ommands" })
 
 nnoremap("<leader>/", function()
-	require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-		previewer = false,
-	}))
+  require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+    previewer = false,
+  }))
 end, { desc = "[/] Fuzzily search in current buffer]" })
 
 nnoremap("<leader>ss", function()
-	require("telescope.builtin").spell_suggest(require("telescope.themes").get_dropdown({
-		previewer = false,
-	}))
+  require("telescope.builtin").spell_suggest(require("telescope.themes").get_dropdown({
+    previewer = false,
+  }))
 end, { desc = "[S]earch [S]pelling suggestions" })
 
 -- Vim Illuminate keybinds
 nnoremap("<leader>]", function()
-	illuminate.goto_next_reference()
-	vim.api.nvim_feedkeys("zz", "n", false)
+  illuminate.goto_next_reference()
+  vim.api.nvim_feedkeys("zz", "n", false)
 end, { desc = "Illuminate: Goto next reference" })
 
 nnoremap("<leader>[", function()
-	illuminate.goto_prev_reference()
-	vim.api.nvim_feedkeys("zz", "n", false)
+  illuminate.goto_prev_reference()
+  vim.api.nvim_feedkeys("zz", "n", false)
 end, { desc = "Illuminate: Goto previous reference" })
 
 -- nvim-ufo keybinds

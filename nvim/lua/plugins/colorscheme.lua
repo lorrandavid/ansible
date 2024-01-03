71ab5b5eb23439bd2bc318be63884cb10a42360b
lorran.thierry.david <lorran.thierry.david@accenture.com>
2024-01-03 09:37:26 -0300

    add copilot & colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
 	{
-		"folke/tokyonight.nvim",
+		'rose-pine/neovim',
+		name = 'rose-pine',
 		lazy = false,
 		priority = 1000,
-		opts = {},
-		config = function()
-			vim.cmd.colorscheme("tokyonight-moon")
-		end,
+		config = function ()
+			vim.cmd.colorscheme('rose-pine')
+		end
 	},
 }

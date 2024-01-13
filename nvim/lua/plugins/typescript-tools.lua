4cf8ff6979c6f34bc577f80627e3d5751e9ffef9
Lorran David <lorrantd1@gmail.com>
2024-01-13 18:41:19 -0300

    update plugins

--- nvim/lua/plugins/typescript-tools.lua
+++
@@ -1,8 +0,0 @@
-return {
-	{
-		"pmizio/typescript-tools.nvim",
-		enable = false,
-		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig", "VonHeikemen/lsp-zero.nvim" },
-		opts = {},
-	},
-}
